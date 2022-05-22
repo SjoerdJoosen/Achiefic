e1cbde91d3d5aea227487e1f35d4056c1da30d1f
SjoerdJoosen <452774@student.fontys.nl>
2022-05-23 01:20:17 +0200

    Refactor existing code

--- Story-Backend/src/main/java/com/example/storybackend/story/Story.java
+++ Story-Backend/src/main/java/com/example/storybackend/story/Story.java
@@ -1,134 +1,43 @@
 package com.example.storybackend.story;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
-@Table
+@Table (name = "story")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Story {
 
     @Id
-    @SequenceGenerator(
-            name = "story_sequence",
-            sequenceName = "story_sequence",
-            allocationSize = 1
-    )
     @GeneratedValue(
-            strategy = GenerationType.SEQUENCE, generator = "story_sequence"
+            strategy = GenerationType.IDENTITY
     )
-    private Long storyId;
+    public Long storyId;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "author")
     private String author;
+
+    @Column(name = "genre")
     private String genre;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "wordCount")
     private Integer wordCount;
+
+    @Column(name = "ageRating")
     private Integer ageRating;
+
+    @Column(name = "actualStory")
     private String actualStory;
-    private Long id;
-
-    public Story() {
-    }
-
-    public Story(Long storyId, String actualStory) {
-        this.storyId = storyId;
-        this.actualStory = actualStory;
-    }
-
-    public Story(Long storyId, String title, String author, String genre, String description, Integer wordCount, Integer ageRating, String actualStory) {
-        this.storyId = storyId;
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-        this.description = description;
-        this.wordCount = wordCount;
-        this.ageRating = ageRating;
-        this.actualStory = actualStory;
-    }
-
-    public Story(String title, String author, String genre, String description, Integer wordCount, Integer ageRating, String actualStory) {
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-        this.description = description;
-        this.wordCount = wordCount;
-        this.ageRating = ageRating;
-        this.actualStory = actualStory;
-    }
-
-    public Long getStoryId() {
-        return storyId;
-    }
-
-    public void setStoryId(Long storyId) {
-        this.storyId = storyId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getWordCount() {
-        return wordCount;
-    }
-
-    public void setWordCount(Integer wordCount) {
-        this.wordCount = wordCount;
-    }
-
-    public Integer getAgeRating() {
-        return ageRating;
-    }
-
-    public void setAgeRating(Integer ageRating) {
-        this.ageRating = ageRating;
-    }
-
-    public String getActualStory() {
-        return actualStory;
-    }
-
-    public void setActualStory(String actualStory) {
-        this.actualStory = actualStory;
-    }
-
-    @Override
-    public String toString() {
-        return "Story{" +
-                "storyId=" + storyId +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", genre='" + genre + '\'' +
-                ", description='" + description + '\'' +
-                ", wordCount=" + wordCount +
-                ", ageRating=" + ageRating +
-                ", actualStory='" + actualStory + '\'' +
-                '}';
-    }
+    
 }
