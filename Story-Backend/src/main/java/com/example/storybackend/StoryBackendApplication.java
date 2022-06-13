168d346210e6fd3a6ef178f405c67a9b62336c36
SjoerdJoosen <452774@student.fontys.nl>
2022-06-13 10:33:16 +0200

    Update StoryBackendApplication.java

--- Story-Backend/src/main/java/com/example/storybackend/StoryBackendApplication.java
+++ Story-Backend/src/main/java/com/example/storybackend/StoryBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoryBackEndApplication {
+class StoryBackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoryBackEndApplication.class, args);
